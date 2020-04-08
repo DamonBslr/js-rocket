ac0895bcd6f8d1f2bee23f71f761377fb85245a1
DamonBslr <63117642+DamonBslr@users.noreply.github.com>
2020-04-08 20:44:38 +0200

    joo

--- js/rocket.js
+++ js/rocket.js
@@ -7,24 +7,41 @@ var changeState = function (state) {
 		countdownNumber = 10;
 		document.getElementById('countdown').innerHTML = countdownNumber;
 
-	if (state == 2) {
+//countdown
+if (state == 2) {
 		timer = setInterval(function(){
 			countdownNumber = countdownNumber-1; document.getElementById('countdown').innerHTML = countdownNumber;
+
+//cant wait!
+if (countdownNumber > 1 && countdownNumber <= 4) {
+	document.getElementById('cant-wait').className = 'cant-wait show'
+	} else{
+	document.getElementById('cant-wait').className = 'cant-wait'
+}
+
+//I'm nervous!
+if (countdownNumber > 4 && countdownNumber <= 7) {
+	document.getElementById('nervous').className = 'nervous show'
+} else{
+	document.getElementById('nervous').className = 'nervous'
+}
+
+//
 if (countdownNumber <= 0) {
 	changeState(3);
 			}
 		}, 500);
 	} else if (state == 3) {
-		var success = setTimeout(function ()
-		{
+		var success = setTimeout(function (){
 			var randomNumber = Math.round(Math.random()*10);
 
 			console.log('randomNumber:',randomNumber)
 
-			if (randomNumber > 3) {
-				changeState(4);
-			} else
-				changeState(5);
+// success
+if (randomNumber > 3){
+	changeState(4);} 
+else
+	changeState(5);
 
 		}, 2000);
 	}
